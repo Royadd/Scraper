93c1f58ed2f2c3b95282cdf51702e0f75810082f
Royadd <30876556+Royadd@users.noreply.github.com>
2018-01-30 00:08:17 +0530

    Update rediffscraper.py
    This includes dates of the headlines also but one headline of 22/01/2018 containing Utkarsh Mishra in bold is not shown properly in the output.

--- rediffscraper.py
+++ rediffscraper.py
@@ -1,7 +1,6 @@
 import re
 import urllib2
 from bs4 import BeautifulSoup
-i=0
 date=raw_input("Enter the date in (DD/MM/YYYY) format to view the headlines: ")
 day,month,year=date.split('/')
 print("Printing the headlines of "+date+" :")
@@ -10,9 +9,7 @@ soup = BeautifulSoup(page,'html.parser')
 d=soup.find_all('div', attrs={'id': 'hdtab1'})
 for x in d:
 	y=x.find('a',attrs={'target': '_new'})
-	i=i+1
-	print(str(i)+". "+y.text[6:])
+	print("-"+y.text[6:])
 	y=x.find_all('a',attrs={'target': '_new','href': re.compile("http://www.rediff.com/news/")})	
 	for z in y:
-		i=i+1
-		print (str(i)+". "+z.text)
+		print ("-"+z.text+z.nextSibling)#+z.nextElementSibling.nextSibling
